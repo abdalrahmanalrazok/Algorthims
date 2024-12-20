5683369d024b3c44074c019e9912adda89252e65
LINDOWS <yss9887@gmail.com>
2024-12-20 23:52:09 +0300

    Add project files.

---
+++ algorthims/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace algorthims
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ pinaryserch/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace algorthims
+{
+    internal class Program
+    {
+        static int punarysrech(int[] ary, int target)
+        {
+            int low = 0, high = ary.Length - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (ary[mid] == target) return mid;
+                if (ary[mid] < target) low = mid + 1;
+                else high = mid - 1;
+            }
+            return -1;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"This is targer {punarysrech([1, 6, 9], 9)}");
+        }
+    }
+}
